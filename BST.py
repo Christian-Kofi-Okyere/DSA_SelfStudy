96591a8f062c424e68fb0a8e85ad87190770788c
Christian <cokyer26@colby.edu>
2023-01-22 21:44:38 -0500

    Add BST file via upload

---
+++ BST.py
@@ -0,0 +1,49 @@
+# take note of the BST properties
+# 1. The value of a node must be graeter than all the values to its left AND must be less than or equak to all the values to its right
+# 2. The left and right children nodes have to also be BSTs
+# main methods: Insertion, Searching and Deletion
+# average time complexity is O(log(N)) , worst case time = O(N)
+
+
+# For insert: Initialize a variable to keep track of what node we are on.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        currentNode = self
+        while True:
+            if value < currentNode.value: #value refers to the value we want to insert
+                if currentNode.left is None:  #if end of branch
+                    currentNode.left = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.left
+            else:
+                if currentNode.right is None:
+                    currentNode.right = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.right
+        return self
+
+
+    def contains(self, value):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                currentNode = currentNode.left
+            elif currentNode > currentNode.value:
+                currentNode = currentNode.right
+            else:
+                return True
+        return False
+
+
+    def remove(self, value, parentNode):
+        return self
+
+    def naver(self):
+        pass
